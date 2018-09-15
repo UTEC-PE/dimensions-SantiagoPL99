f5b6b549caee4971948161bbdf11836da429772a
SantiagoPL99 <42584008+SantiagoPL99@users.noreply.github.com>
2018-09-14 19:51:55 -0500

    main.cpp y vector.h

--- main.cpp
+++ main.cpp
@@ -6,18 +6,34 @@
 using namespace std;
 
 template <typename T>
-struct Dimensions {      
-    int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+struct Dimensions
+{
+    int operator()(int* coordinates, int* sizes, int dimensions)
+    {
+        float acum = 1;
+        float sum = coordinates[0];
+        for (int i = 0; i < dimensions-1; i++)
+        {
+            acum *= sizes[i];
+            sum += acum*coordinates[i+1];
+        }
     }
 };
 
-struct Integer {
+struct Integer
+{
        typedef int T;
        typedef Dimensions<T> Operation;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int dimensionSizes[2]={3,3};
+    int dimensions = 2;
+    int coordinates[2]={1,1};
+    Vector<Integer> vector(dimensions, dimensionSizes);
+    vector.set(2,coordinates):
+
     system("pause");
     return EXIT_SUCCESS;
 }
--- vector.h
+++ vector.h
@@ -17,13 +17,24 @@ class Vector {
     public:
         Vector() : data(nullptr) {};
              
-        Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
-            // TODO
+        Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes)
+        {
+            dataSize=1;
+            for(int i = 0; i<dimensions;i++)
+            {
+                dataSize *= dimensionSizes[i];
+            }
         }
              
-        void set(T datum, int* coordinates); // TODO
+        void set(T datum, int* coordinates)
+        {
+            // TODO
+        };
              
-        T get(int* coordinates); // TODO
+        T get(int* coordinates)
+        {
+            // TODO
+        };
 };
 
 #endif
